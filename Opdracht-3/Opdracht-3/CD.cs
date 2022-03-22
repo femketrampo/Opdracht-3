a62a513bb9a62e2df1a742598d95935133f69793
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:48:07 +0100

    Add files via upload

---
+++ Opdracht-3/Opdracht-3/CD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_3
+{
+    class CD
+    {
+        public string uitvoerder;
+        public string titel;
+
+        public CD(string Uitvoerder, string Titel)
+        {
+            uitvoerder = Uitvoerder;
+            titel = Titel;
+        }
+        public CD()
+        {
+            titel = "Titel";
+            uitvoerder = "Uitvoerder";
+        }
+        //REMEMBER!!
+        //Optie 2: (check main)
+
+        //public override string ToString()
+        //{
+        //    return this.titel + " - " + this.uitvoerder;
+        //}
+    }
+}
